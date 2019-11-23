e7cc2da6c1ae13bcad19069ed257185af5ed5224
Fábio Pereira Barros <fpbbarros@gmail.com>
2019-11-23 00:53:29 -0300

    criacao dos metodos da model categoria

---
+++ source/models/Category.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace Models;
+
+use Core\Connection;
+
+class Category
+{
+
+  //busca
+  public static function get(int $id = null, string $code = null)
+  {
+    if (!is_null($code)) {
+      return Connection::table('category')->select('*')->where('code', $code)->get();
+    }
+    if (!is_null($code)) {
+      return Connection::table('category')->find($id);
+    } else {
+      return Connection::select('select * from category');
+    }
+  }
+
+  public static function byId(int $id)
+  {
+    if (!is_null($id)) {
+      return Connection::table('category')->select('*')->where('id', $id)->get()->count();
+    }
+  }
+
+  public static function byName(string $name, int $id = null)
+  {
+    if (empty($id)) {
+      return Connection::table('category')->select('*')->where('name', $name)->get()->count();
+    } else {
+      return Connection::table('category')->select('*')->where('name', $name)->where('id', '<>', $id)->get()->count();
+    }
+  }
+
+  public static function byCode(string $code, int $id = null)
+  {
+    if (empty($id)) {
+      return Connection::table('category')->select('*')->where('code', $code)->get()->count();
+    } else {
+      return Connection::table('category')->select('*')->where('code', $code)->where('id', '<>', $id)->get()->count();
+    }
+  }
+
+  //verificacao
+  public static function validate(array $input): bool
+  {
+    $validation = true;
+    if (isset($input['name']) && strlen($input['name']) < 1) {
+      UILogger::in('invalid name!', 2);
+      $validation = false;
+    }
+    if (isset($input['code']) && strlen($input['code']) < 1) {
+      UILogger::in('invalid code!', 2);
+      $validation = false;
+    }
+    return $validation;
+  }
+
+
+  //crud
+  public static function insert(array $input): int
+  {
+    if (self::byName($input['name']) != 0 || self::byCode($input['code']) != 0) {
+      return false;
+    }
+    if (!self::validate(($input))) {
+      return false;
+    }
+    if ($id = Connection::table('category')->insertGetId(['name' => $input['name'], 'code' => $input['code']])) {
+      UILogger::in('Registered category', 1);
+      return $id;
+    }
+    UILogger::in('not registered', 2);
+    return false;
+  }
+
+
+  public static function update(array $data): bool
+  {
+    if (self::byName($data['name'], $data['id']) != 0 && self::byCode($data['code'], $data['id']) != 0) {
+      return false;
+    }
+    if (!self::validate(($data))) {
+      return false;
+    }
+    if (Connection::table('category')->where('id', $data['id'])->update(['name' => $data['name'], 'code' => $data['code']])) {
+      UILogger::in('updated category', 1);
+      return true;
+    }
+    UILogger::in('not updated', 2);
+    return false;
+  }
+
+  public static function delete(int $id): bool
+  {
+    if (self::byId($id) > 0) {
+      if (Connection::table('category')->where('id', $id)->delete()) {
+        UILogger::in('deleted category', 2);
+        return true;
+      }
+    }
+    UILogger::in('not deleted!', 2);
+    return false;
+  }
+}
