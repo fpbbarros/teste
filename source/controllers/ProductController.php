6861afcbbb3ef95828a50f5afc500127345d1234
Fábio Pereira Barros <fpbbarros@gmail.com>
2019-11-24 02:19:06 -0300

    inclusao do controller de Product

--- source/controllers/ProductController.php
+++ source/controllers/ProductController.php
@@ -2,10 +2,28 @@
 
 namespace Controllers;
 
+use Helpers\ObjToJson;
+use Models\Product;
+
 class ProductController
 {
   public static function get(int $id = null)
-  { 
-    
+  {
+    return ObjToJson::objToJson(Product::byId($id));
+  }
+
+  public static function insert(array $data)
+  {
+    return Product::insert($data);
+  }
+
+  public static function update(array $data)
+  {
+    return Product::update($data);
+  }
+
+  public static function delete(int $data)
+  {
+    return Product::delete($data);
   }
 }
